bd8764d144c2345fe4a180b6ad5a9975cbc4627d
Lucas Alves <138324870+tluccas@users.noreply.github.com>
2025-06-26 17:05:44 -0300

    feat: subindo os Slash Commands para todos os servidores

--- src/main/java/org/example/views/ReadyListener.java
+++ src/main/java/org/example/views/ReadyListener.java
@@ -10,17 +10,16 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 public class ReadyListener extends ListenerAdapter {
     @Override
     public void onReady(ReadyEvent event) {
-        event.getJDA().getPresence().setActivity(Activity.playing("*help <a:idontcare:1387821007955296256>"));
+        event.getJDA().getPresence().setActivity(Activity.playing("*help \uD83E\uDD16"));
         System.out.println("Bot está pronto e com presença atualizada!");
-        // Registrar comandos slash no void (para testes)
-        Guild guild = event.getJDA().getGuildById("1113547243593678979");
-        if (guild != null) {
-            guild.updateCommands().addCommands(
+        /*// Registrar comandos slash no void (para testes)
+        Guild guild = event.getJDA().getGuildById("1113547243593678979");*/
+            event.getJDA().updateCommands().addCommands(
                     Commands.slash("help", "Exibe os comandos disponíveis"),
                     Commands.slash("ping", "Responde com pong"),
                     Commands.slash("astra", "Vai te responder utilizando IA <a:robo:1387819870321905684>")
                             .addOption(OptionType.STRING, "olá", "Me fale algo", true)
             ).queue();
-        }
+
     }
 }
